99970312081b507c7551a8eb47b1da02aa6598fc
love jaiswal <lovejaiswal881@gmail.com>
2026-08-06 19:55:45 +0530

    new added

--- app.py
+++ app.py
@@ -7,25 +7,22 @@ from routes.auth import auth
 from flasgger import Swagger
 
 app = Flask(__name__)
-Swagger(app)
 
-app.register_blueprint(student)
-
-# Load configuration
 app.config.from_object(Config)
 
-# Initialize extensions
 db.init_app(app)
 jwt.init_app(app)
 
-# Register blueprints
+Swagger(app)
+
 app.register_blueprint(auth)
+app.register_blueprint(student)
 
 @app.route("/")
 def home():
-    return {
-        "message": "Student API is Live 🚀"
-    }
+    return {"message": "Student API is Live 🚀"}
+
+
 
 if __name__ == "__main__":
     with app.app_context():
